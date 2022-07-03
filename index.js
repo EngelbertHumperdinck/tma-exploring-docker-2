eec78cd00077cf3ef619be65c9a0c4cc209b4008
BigSteveLittle <bigsteve@redandblackzone.com>
2022-07-03 21:21:54 +1000

    Adds 'body-parser' to the index.js and package.json files and makes formating changes to index.ejs.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Connect to MongoDB
 mongoose
